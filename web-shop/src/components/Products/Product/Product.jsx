4f8c8dee7f3c7482cf47b0f0159838276618be8c
Luiz Pedro <luizpedro.pedrosilva@gmail.com>
2022-01-29 18:38:08 -0300

    construct-product

--- web-shop/src/components/Products/Product/Product.jsx
+++ web-shop/src/components/Products/Product/Product.jsx
@@ -1,26 +1,35 @@
 import React from 'react';
 
 //layout dos produtos
-import { Card, CardMedia, CardContent, CardActions, Typography, IconBotton } from '@material-ui/core';
+import { Card, CardMedia, CardContent, CardActions, Typography, IconBotton, CardActionArea } from '@material-ui/core';
 
 //carrinho
 import { AddShoppingCart } from '@material-ui/icons';
 
-const Product = () => {
-  return <div className={classes.root}>
-    <CardMedia className={classes.media} image='' title={product.name}/>
-    <CardContent>
-        <div className={classes.cardContent}>
-            <Typography vaeiant='h5' gutterBottom>
-                {product.name}
+const Product = ({product}) => {
+  return( 
+    <Card className={classes.root}>
+        <CardMedia className={classes.media} image='' title={product.name}/>
+        <CardContent>
+            <div className={classes.cardContent}>
+                <Typography variant='h5' gutterBottom>
+                    {product.name}
+                </Typography>
+                <Typography vaeiant='h5' gutterBottom>
+                    {product.price}
+                </Typography>
+            </div>
+            <Typography vaeiant='h2' color='textSecondary'>{product.description}
+                    {product.price}
             </Typography>
-            <Typography vaeiant='h5' gutterBottom>
-                {product.price}
-            </Typography>
-        </div>
-    </CardContent>
-
-  </div>;
-};
+        </CardContent>
+        <CardActions disableSpacing className={classes.CardActions}>
+            <IconBotton Aria-label="Add to Cart">
+                <AddShoppingCart />
+            </IconBotton>
+        </CardActions>
+    </Card>
+  );
+}
 
 export default Product;
