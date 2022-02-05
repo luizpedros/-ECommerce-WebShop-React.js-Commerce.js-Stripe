16c81bd841a1a6fb0b65ce4ed3aed276a6d5db68
Luiz Pedro <luizpedro.pedrosilva@gmail.com>
2022-02-04 22:30:42 -0300

    funcionalidade-ir-carrinho-volta-home

--- web-shop/src/App.js
+++ web-shop/src/App.js
@@ -1,9 +1,8 @@
 //criar componete funcional usando "rafce" que nos permite criar com facilidade componetes funcionais
 import React, { useState, useEffect } from "react";
 import { commerce } from "./lib/commerce";
-
 import{Products, Navbar, Cart} from './components';
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 
 const App = () => {
@@ -36,12 +35,20 @@ const App = () => {
     console.log(cart)
 
     return(
-        <div>
-            <Navbar totalItems={cart.total_items}/>
-            {/*<Products products={products}  onAddToCart={handleAddToCart}/>*/}
-            <Cart cart={cart}/>
-        </div>
-    )
+        <Router>        
+            <div>
+                <Navbar totalItems={cart.total_items}/>
+                <Switch>
+                    <Route exact path="/">
+                        <Products products={products} onAddToCart={handleAddToCart}/>
+                    </Route>
+                    <Route exact path="/cart">
+                        <Cart cart={cart}/>
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default App; 
--- web-shop/src/components/Navbar/Navbar.jsx
+++ web-shop/src/components/Navbar/Navbar.jsx
@@ -1,35 +1,34 @@
 //Barra de navegação responsiva
 import React from 'react';
-
 import { AppBar, Toolbar, IconButton, Badge, ManuItem, Menu, Typography } from '@material-ui/core';
-
 import { ShoppingCart } from '@material-ui/icons';
-
 //logo tipo
 import logo from '../../assets/shop-icon.png'
-//estilos
 import useStyles from './styles';
+import { Link, useLocation } from 'react-router-dom';
+
 
 const Navbar = ({ totalItems }) => {
     const classes = useStyles();
-
+    const location = useLocation();  
 
   return( 
   <div>
       <AppBar position='fixed' className={classes.AppBar} color="inherit"> 
         <Toolbar>
-            <Typography variant='h6' className={classes.title} color='inherit'>
+            <Typography component={Link} to='/' variant='h6' className={classes.title} color='inherit'>
                 <img src={logo} about='client' height='25px' className={classes.image} />
                 Comercil
             </Typography>
             <div className={classes.grow}/>
+            {location.pathname === '/' &&(
             <div className={classes.button}>
-                <IconButton aria-label='Show cart items' color='inherit'>
+                <IconButton component={Link} to='/cart' aria-label='Show cart items' color='inherit'>
                     <Badge badgeContent={totalItems} color='secondary'>
                         <ShoppingCart />
                     </Badge>
                 </IconButton>
-            </div>
+            </div>)}
 
         </Toolbar>
       </AppBar>
